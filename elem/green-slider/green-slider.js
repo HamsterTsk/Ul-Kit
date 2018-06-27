e4568eb03d7dbbf6121813926c9e4b2aab405761
HamsterTsk <HamsterTsk@users.noreply.github.com>
2018-06-27 19:59:06 +0700

    create green-slider element

---
+++ elem/green-slider/green-slider.js
@@ -0,0 +1,99 @@
+var greenSliderCircle = document.getElementById('green-slider__circle');
+var greenSlider = document.getElementById('green-slider');
+var greenSliderBarActive = document.getElementById('green-slider__bar_active');
+var greenSliderNumb2 = document.getElementById('green-slider__number_2');
+var greenSliderNumb3 = document.getElementById('green-slider__number_3');
+var greenSliderNumb4 = document.getElementById('green-slider__number_4');
+var greenSliderNumb5 = document.getElementById('green-slider__number_5');
+greenSliderCircle.onmousedown = function(ev) {
+    var circleCoords = getCoords(greenSliderCircle);
+    var shiftX = ev.pageX - circleCoords.left;
+    var sliderCoords = getCoords(greenSlider);
+    document.onmousemove = function(ev) {
+        var newLeft = ev.pageX - shiftX - sliderCoords.left;
+        if ( newLeft < 0 ) {
+            newLeft = 0;
+        }
+        var rightEdge = greenSlider.offsetWidth - greenSliderCircle.offsetWidth;
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+        greenSliderCircle.style.left = newLeft + 'px';
+        greenSliderBarActive.style.width = newLeft + 5 + 'px';
+        if ( newLeft >= 60 ) {
+            greenSliderNumb2.style.color = '#4db7a7';
+        }
+        if ( newLeft < 60 ) {
+            greenSliderNumb2.style.color = 'rgb(209,209,209)';
+        }
+        if ( newLeft >= 125 ) {
+            greenSliderNumb3.style.color = '#4db7a7';
+        }
+        if ( newLeft < 125 ) {
+            greenSliderNumb3.style.color = 'rgb(209,209,209)';
+        }
+        if ( newLeft >= 190 ) {
+            greenSliderNumb4.style.color = '#4db7a7';
+        }
+        if ( newLeft < 190 ) {
+            greenSliderNumb4.style.color = 'rgb(209,209,209)';
+        }
+        if ( newLeft >= 240 ) {
+            greenSliderNumb5.style.color = '#4db7a7';
+        }
+        if ( newLeft < 240 ) {
+            greenSliderNumb5.style.color = 'rgb(209,209,209)';
+        }
+    };
+    document.onmouseup = function() {
+        document.onmousemove = document.onmouseup = null;
+    };
+    return false;
+};
+greenSliderCircle.ondragstart = function() {
+    return false;
+};
+greenSlider.onclick = function(ev) {
+    var newLeft = ev.clientX - 15;
+    var rightEdge = greenSlider.offsetWidth - greenSliderCircle.offsetWidth;
+    if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+    }
+    if ( newLeft < 0 ) {
+        newLeft = 0;
+    }
+    greenSliderCircle.style.left = newLeft + 'px';
+    greenSliderBarActive.style.width = newLeft + 'px';
+    if ( newLeft >= 60 ) {
+        greenSliderNumb2.style.color = '#4db7a7';
+    }
+    if ( newLeft < 60 ) {
+        greenSliderNumb2.style.color = 'rgb(209,209,209)';
+    }
+    if ( newLeft >= 125 ) {
+        greenSliderNumb3.style.color = '#4db7a7';
+    }
+    if ( newLeft < 125 ) {
+        greenSliderNumb3.style.color = 'rgb(209,209,209)';
+    }
+    if ( newLeft >= 190 ) {
+        greenSliderNumb4.style.color = '#4db7a7';
+    }
+    if ( newLeft < 190 ) {
+        greenSliderNumb4.style.color = 'rgb(209,209,209)';
+    }
+    if ( newLeft >= 248 ) {
+        greenSliderNumb5.style.color = '#4db7a7';
+    }
+    if ( newLeft < 248 ) {
+        greenSliderNumb5.style.color = 'rgb(209,209,209)';
+    }
+    return false;
+};
+function getCoords(elem) { // кроме IE8-
+    var box = elem.getBoundingClientRect();
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+}
