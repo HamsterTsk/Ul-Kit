097f37d76f8f88ce035a8e666bff45c2667f2f12
HamsterTsk <HamsterTsk@users.noreply.github.com>
2018-06-29 17:05:51 +0700

    create events element

---
+++ elem/events/events.js
@@ -0,0 +1,11 @@
+var eventsButton = document.getElementById('events__button');
+
+
+eventsButton.onmouseup = function() {
+    eventsButton.style.boxShadow = '0 3px 0 0 #369284';
+    eventsButton.style.marginTop = '20px';
+};
+eventsButton.onmousedown = function() {
+    eventsButton.style.boxShadow = '0 0 0 0';
+    eventsButton.style.marginTop = '23px';
+};
